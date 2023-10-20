385070835848fd8370c37254d8969637204c1afa
fahrifaizin <fahrifaizin98@gmail.com>
2023-10-20 13:46:03 +0700

    Tugas5'

--- index3.js
+++ index3.js
@@ -1,48 +1,68 @@
-const readline = require('readline');
-const validator = require('validator');
-
+const readline = require("readline");
+const validator = require("validator");
+const fs = require("fs");
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-let nama = '';
-let handphone = '';
+//membuat fungsi create folder atau file json jika tidak tersedia/dihapus
+const dirPath = "./data";
+if (!fs.existsSync(dirPath)) {
+  fs.mkdirSync(dirPath);
+}
+//file json
+const filePath = "./data/contacts.json";
+if (!fs.existsSync(filePath)) {
+  fs.writeFileSync(filePath, "[]", "utf-8");
+}
 
 function inputNama() {
-    rl.question("Masukkan nama anda ? ", (inputNama) => {
-        if (validator.isLength(inputNama, { min: 3, max: 50 })) {
-            nama = inputNama;
-            inputNomorHandphone();
-        } else {
-            console.log("Nama harus memiliki panjang minimal 3 karakter dan maksimal 50 karakter. Silakan coba lagi.");
-            inputNama();
-        }
-    });
+  rl.question("Masukkan Nama : ", (nama) => {
+    inputNotelp(nama);
+  });
 }
 
-function inputNomorHandphone() {
-    rl.question("Masukkan Nomor Handphone anda ? ", (inputHandphone) => {
-        if (validator.isMobilePhone(inputHandphone, "any")) {
-            handphone = inputHandphone;
-            inputEmail();
-        } else {
-            console.log("Nomor Handphone tidak valid. Silakan coba lagi.");
-            inputNomorHandphone();
-        }
-    });
+function inputNotelp(nama) {
+  rl.question("Masukkan NoTelp : ", (telp) => {
+    const cekTelpon = validator.isMobilePhone(telp, "id-ID", { strictMode: false });
+
+    if (cekTelpon) {
+      askEmail(nama, telp);
+    } else {
+      console.log("No Telp salah. Silahkan masukkan sesuai format IDN!");
+      inputNotelp(nama);
+    }
+  });
 }
 
-function inputEmail() {
-    rl.question("Masukkan email anda ? ", (email) => {
-        if (validator.isEmail(email)) {
-            console.log(`Nama saya adalah ${nama}, nomor telepon saya ${handphone}, dan email saya adalah ${email}. Thank You!!`);
-            rl.close();
-        } else {
-            console.log("Alamat email tidak valid. Silakan coba lagi.");
-            inputEmail();
-        }
-    });
+function askEmail(nama, telp) {
+  rl.question("Masukkan Email : ", (email) => {
+    const cekEmail = validator.isEmail(email);
+
+    if (cekEmail) {
+      simpanInfo(nama, telp, email);
+      console.log(`Nama saya ${nama}, nomor telpon saya  ${telp}, dan alamat email saya ${email}`);
+      rl.close();
+    } else {
+      console.log("Email yang dimasukkan salah. Silahkan masukkan sesuai format email");
+      askEmail(nama, telp);
+    }
+  });
 }
+const simpanInfo = (nama, telp, email) => {
+  //menyimpan hasil inputan nama, telp, email kedalam variabel contact
+  const contact = { nama, telp, email };
+
+  //membaca file contact.json
+  const file = fs.readFileSync("data/contacts.json", "utf-8");
+  const contacts = JSON.parse(file);
+
+  //menambahkan daftar kontak baru ke daftar kontak
+  contacts.push(contact);
+
+  //menyimpan atau menulis daftar kontak ke file json
+  fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
+};
 
 inputNama();
